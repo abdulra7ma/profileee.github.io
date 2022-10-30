c1253e1f3996e1a376c70e5c687c0c39c45486f2
Golden abdu <abdulrahman.goldendawn@gmail.com>
2022-10-30 20:39:34 +0600

    added footer and script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let hamburger = document.getElementById('hamburger')
+menu = document.getElementById('menu')
+header = document.getElementById('header')
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active')
+    menu.classList.toggle('active')
+})
+
+AOS.init()
